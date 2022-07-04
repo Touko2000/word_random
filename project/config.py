0386a7c99fc213596b781ddd9f068ed42e4a5b55
liuweikang <liuweikang@zhihu.com>
2022-07-04 23:39:18 +0800

    update

--- project/config.py
+++ project/config.py
@@ -4,7 +4,7 @@ FilePath: /word_random/project/config.py
 Author: lwk
 Date: 2022-06-05 14:35:51
 LastEditors: lwk
-LastEditTime: 2022-07-04 23:38:36
+LastEditTime: 2022-07-04 23:39:04
 '''
 # 词表路径
 cibiao = "/Users/touko/Desktop/pycode/word_random/static/data/要你命3000词表.xlsx"
