34e63b4f5ad9c780b0aa3893545c91b2083d13fb
liuweikang <liuweikang@zhihu.com>
2022-07-04 23:39:01 +0800

    update

--- project/config.py
+++ project/config.py
@@ -4,10 +4,12 @@ FilePath: /word_random/project/config.py
 Author: lwk
 Date: 2022-06-05 14:35:51
 LastEditors: lwk
-LastEditTime: 2022-07-04 23:26:19
+LastEditTime: 2022-07-04 23:38:36
 '''
+# 词表路径
 cibiao = "/Users/touko/Desktop/pycode/word_random/static/data/要你命3000词表.xlsx"
 
-sheets = [29,30,31] # 第几个list
+# 哪几个list
+sheets = [29,30,31] 
 sheets = [f"List{n}" for n in sheets]
 list_name = "_".join(sheets)
